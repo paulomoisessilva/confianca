df3e9deb078c9df7e7143753988fbbf1ef4900ca
Paulo Moisés <paulomoises_silva@hotmail.com>
2019-08-27 23:26:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 var dp = 0.008030567
+$(function() {
+    if ($.cookie("valor")) {
+        $("#fechamento").val($.cookie("valor"))
+        $("#nivel").val($.cookie("ve"))
+        $("#max").html(Math.round($.cookie("max") * 10) / 10)
+        $("#min").html(Math.round($.cookie("min") * 10) / 10)
+    }
+})
 
 $("#nivel, #fechamento").on("change keyup paste", function() {
     var valor = $("#fechamento").val()
@@ -11,6 +19,10 @@ $("#nivel, #fechamento").on("change keyup paste", function() {
         var res = ve * window.dp
         var max = valor * (1 + res)
         var min = valor * (1 - res)
+        $.cookie("valor", valor)
+        $.cookie("ve", ve)
+        $.cookie("max", max)
+        $.cookie("min", min)
         $("#max").html(Math.round(max * 10) / 10)
         $("#min").html(Math.round(min * 10) / 10)
     }
